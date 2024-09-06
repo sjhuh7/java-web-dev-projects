1088333c3e969e7beb28cd78acf8e70ac424f0f1
unknown <sjhuh1999@gmail.com>
2024-09-05 19:51:48 -0500

    studio 1

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a radius:");
+        double radius = input.nextDouble();
+        double pi = 3.14;
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius" + radius + "is" + area);
+        input.close();
+    }
+}
